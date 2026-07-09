bf1f12b291da66e2fd651b0ba5367963d6f70f53
ggbtk <120757678+ggbtk@users.noreply.github.com>
2026-07-09 15:04:39 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 export const siteConfig = {
   siteName: '컨텐츠이용료미납 희망티켓',
   siteUrl: 'https://contents-hh-2.pages.dev',
-  siteDescription: '컨텐츠이용료현금 컨텐츠이용료미납 컨텐츠이용료현금화 콘텐츠용료현금 콘텐츠이용료현금화 콘텐츠이용료미납 정보이용료미납  정보이용료현금 정보이용료현금화 희망티켓',
+  siteDescription: '희망티켓: 콘텐츠 및 정보이용료 현금화 전문, 미납 결제도 빠르고 안전하게 해결해 드립니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.jpg',
   naverVerificationCode: '96e7314de286c49874afd6683ea87fdd51585f63',
